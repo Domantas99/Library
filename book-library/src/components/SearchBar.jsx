1a7cf146453387203143f55c6d9e71ee7af67bea
Domantas99 <Domantas.greicius@gmail.com>
2020-04-03 00:27:50 +0300

    Added navigation on enter key press

--- book-library/src/components/SearchBar.jsx
+++ book-library/src/components/SearchBar.jsx
@@ -2,17 +2,25 @@ import React from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { getSearchedBooks } from '../store/search-bar/actions';
 import SearchBarResultBlock from './SearchBarResultBlock';
+import { useHistory } from 'react-router-dom';
 
 export default function SearchBar() {
   const dispatch = useDispatch();
   const books = useSelector(state => state.searchbar.data);
-  
-  function  search(value) {
+  const history = useHistory();
+
+  function search(value) {
     dispatch(getSearchedBooks(value));
   }
 
+  function navigationOnEnter() {
+    if(books[0] != null) {
+      history.push('/library/' + books[0].id);
+    }
+  } 
+
   return (
-    <div className="searchbar"> 
+    <div onKeyPress= {e => e.key === 'Enter' && navigationOnEnter() } className="searchbar"> 
         <input className="searchbar-input" onChange={(e) => search(e.target.value)} placeholder="Search books"></input>
         <div className="searchbar-results">
           {
