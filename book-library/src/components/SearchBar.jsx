9ff50e5df6a54df3f90748f0be9925e669aac5af
Domantas99 <Domantas.greicius@gmail.com>
2020-04-03 00:08:46 +0300

    Improved styling

--- book-library/src/components/SearchBar.jsx
+++ book-library/src/components/SearchBar.jsx
@@ -1,27 +1,24 @@
-import React, { useState } from 'react';
+import React from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { getSearchedBooks } from '../store/search-bar/actions';
+import SearchBarResultBlock from './SearchBarResultBlock';
 
 export default function SearchBar() {
   const dispatch = useDispatch();
   const books = useSelector(state => state.searchbar.data);
-  let timerId;
+  
   function  search(value) {
    dispatch(getSearchedBooks(value));
   }
 
-  function debounce(func, delay) {
-    clearTimeout(timerId);
-    timerId = setTimeout(func, delay);
-  }
-
   return (
-    <div>
-      {console.log(books, 'cia state')}
-        <input onChange={(e) => search(e.target.value)} placeholder="Search books"></input>
-        {books.map(b => (
-          <div key={b.id}>{b.title}</div>
-        ))}
+    <div className="searchbar"> 
+        <input className="searchbar-input" onChange={(e) => search(e.target.value)} placeholder="Search books"></input>
+        <div className="searchbar-results">
+          {
+            books.map(book => (<SearchBarResultBlock key={book.id} book={book}></SearchBarResultBlock>))
+          }
+        </div>
     </div>
   );
 }
--- book-library/src/components/SearchBarResultBlock.jsx
+++ book-library/src/components/SearchBarResultBlock.jsx
@@ -1,10 +1,23 @@
 import React from 'react'
 
-export default function SearchResultBlock({book}) {
+export default function SearchBarResultBlock({book}) {
     return (
-        <div>
-            <h3>{book.id}</h3>
-            <h3>{book.title}</h3>
+        <div className="search-block">
+            {console.log(book, 'cia book')}
+            <div>
+                <img className="search-block__image"
+                    // src={book.coverPictureUrl} 
+                    src="https://dummyimage.com/100x135.jpg/dddddd/000000" 
+                    alt="https://dummyimage.com/100x135.jpg/dddddd/000000"/>
+            </div>
+            <div>
+                <h1 className="search-block-title">{book.title}</h1>
+                <h3 className="search-block-isbn">{book.isbn}</h3>
+                <h2 className="search-block-other">{book.author}</h2>
+                <h2 className="search-block-other">Stars</h2>
+                <h2 className="search-block-other">Available</h2>
+                
+            </div>
         </div>
     )
 }
