9ad5b4736ae72ff2eec7d01a0cfff630a5489495
Karolis Vaitelė <karvai11@ktu.lt>
2020-04-05 15:10:29 +0300

    Removed useless files

--- book-library/src/components/TextInputBlock.jsx
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-
-export default ({ label, inputName }) => (
-    <div className="input-block">
-        <label for={inputName} class="form-label">{label}</label><br/>
-        <input type="text" name={inputName} id={inputName}/><br/>
-    </div>
-);
