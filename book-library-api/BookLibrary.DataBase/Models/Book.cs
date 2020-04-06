1dc4d6f54b0e0ffa6dd982fa9d90c694a1d635e6
Domantas99 <Domantas.greicius@gmail.com>
2020-04-07 00:00:36 +0300

    Reverted book model changes

--- book-library-api/BookLibrary.DataBase/Models/Book.cs
+++ book-library-api/BookLibrary.DataBase/Models/Book.cs
@@ -22,7 +22,9 @@ namespace BookLibrary.DataBase.Models
         public string Tag { get; set; }
         public string CoverPictureUrl { get; set; }
         public string GoodReadsUrl { get; set; }
+        [Column(TypeName ="Date")]
         public DateTime DateAdded { get; set; }
+        [Column(TypeName = "Date")]
         public DateTime ReleaseDate { get; set; }
         public virtual Wish Wish { get; set; }
         public virtual ICollection<BookComment> BookComment { get; set; }
