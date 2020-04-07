7a824302d1ba0ad3d6c443de358620363b876279
Audrius Frankonis <audrius.frankonis@gmail.com>
2020-04-07 14:26:26 +0300

    Add new styles to book list Clean up missing imports & placeholder CSS Rearrange HTML to better match prototype and CSS

--- book-library/src/components/BookListItem.jsx
+++ book-library/src/components/BookListItem.jsx
@@ -1,11 +1,11 @@
 import React from "react";
 
 export default ({data}) => (
-    <div className="book-list-item" id={`book-list-item-${data['Id']}`}>
-        <a href={`/library/${data['Id']}`}>
-            <img className="book-list-item--cover" src={data['CoverPictureUrl']} alt=""></img>
-            <span className="book-list-item--title">{data["Title"]}</span>
-        </a>
-        <span className="book-list-item--author">{data["Author"]}</span>
-    </div>
+    <a className="book" id={`book-${data['Id']}`} href={`/library/${data['Id']}`}>
+        <div className="book__image">
+            <img src={data['CoverPictureUrl']} alt=""/>
+        </div>
+        <span className="book__title">{data["Title"]}</span>
+        <span className="book__author">{data["Author"]}</span>
+    </a>
 )
