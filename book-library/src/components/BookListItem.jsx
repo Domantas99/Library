281de1bd146cd2f6bf9c88e893080ab63a170163
Domantas99 <Domantas.greicius@gmail.com>
2020-04-22 18:48:11 +0300

    Refactoring

--- book-library/src/components/BookListItem.jsx
+++ book-library/src/components/BookListItem.jsx
@@ -5,7 +5,10 @@ import { Link } from "react-router-dom";
 export default ({ data }) => (
   <Link to={`/library/${data.id}`} className="book" id={`book-${data.id}`}>
     <div className="book__image">
-      <img src="https://picsum.photos/200/300?random=1" alt="" />
+      <img
+        src={data.coverPictureUrl}
+        alt="https://picsum.photos/200/300?random=1"
+      />
     </div>
     <span className="book__title">{data.title}</span>
     <span className="book__author">{data.author}</span>
