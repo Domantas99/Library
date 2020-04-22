596ad6f5f98f5dff57a6b41843897b537455a76c
Domantas99 <Domantas.greicius@gmail.com>
2020-04-22 12:22:02 +0300

    Refactoring, used reusable components

--- book-library/src/components/BookListItem.jsx
+++ book-library/src/components/BookListItem.jsx
@@ -1,11 +1,13 @@
+/* eslint-disable react/prop-types */
 import React from "react";
+import { Link } from "react-router-dom";
 
-export default ({data}) => (
-    <a className="book" id={`book-${data['Id']}`} href={`/library/${data['Id']}`}>
-        <div className="book__image">
-            <img src={data['CoverPictureUrl']} alt=""/>
-        </div>
-        <span className="book__title">{data["Title"]}</span>
-        <span className="book__author">{data["Author"]}</span>
-    </a>
-)
+export default ({ data }) => (
+  <Link to={`/library/${data.id}`} className="book" id={`book-${data.id}`}>
+    <div className="book__image">
+      <img src="https://picsum.photos/200/300?random=1" alt="" />
+    </div>
+    <span className="book__title">{data.title}</span>
+    <span className="book__author">{data.author}</span>
+  </Link>
+);
--- book-library/src/components/BooksSection.jsx
+++ book-library/src/components/BooksSection.jsx
@@ -1,27 +1,21 @@
 /* eslint-disable react/prop-types */
 import React from "react";
 import { Link } from "react-router-dom";
+import BookListItem from "./BookListItem";
 
 export default function BooksSection({ books, sectionName }) {
   return (
     <div className="panel">
       <div className="panel__header book-section-header">
         <h1>{sectionName}</h1>
-        <Link className="book-section-header-link" to="/library">See all</Link>
+        <Link className="book-section-header-link" to="/library">
+          See all
+        </Link>
       </div>
       <div className="panel__content book-grid book-section" data-copies="4">
         {books.map((book) => (
-          <div key={book.id}>
-            <div className="book-section-block">
-              <div className="book-section-block-image">
-                <img src="https://picsum.photos/200/300?random=1" alt="" />
-              </div>
-              <div>
-                <div className="book-section-block-title">{book.title}</div>
-                <div className="book-section-block-author">{book.author}</div>
-                {/* <span>Rating</span> */}
-              </div>
-            </div>
+          <div className="book-section-block">
+            <BookListItem data={book} />
           </div>
         ))}
       </div>
