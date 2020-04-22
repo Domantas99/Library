f93065917fc0555b74fdc89b8ec768afb5987ea3
Domantas99 <Domantas.greicius@gmail.com>
2020-04-23 00:46:27 +0300

    removed alt text

--- book-library/src/components/BookListItem.jsx
+++ book-library/src/components/BookListItem.jsx
@@ -5,10 +5,7 @@ import { Link } from "react-router-dom";
 export default ({ data }) => (
   <Link to={`/library/${data.id}`} className="book" id={`book-${data.id}`}>
     <div className="book__image">
-      <img
-        src={data.coverPictureUrl}
-        alt="https://picsum.photos/200/300?random=1"
-      />
+      <img src={data.coverPictureUrl} alt="" />
     </div>
     <span className="book__title">{data.title}</span>
     <span className="book__author">{data.author}</span>
