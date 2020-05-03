61c7ed721981569ae2a53b4fd40b14f51205cabd
Audrius Frankonis <audrius.frankonis@gmail.com>
2020-05-03 17:59:11 +0300

    Create reservation/waitlist for book details

--- book-library/src/store/reservations/reducer.js
+++ book-library/src/store/reservations/reducer.js
@@ -1,7 +1,8 @@
-import { GET_RESERVATIONS_END } from "./actionTypes";
+import { GET_RESERVATIONS_END, GET_BOOK_RESERVATIONS_END } from "./actionTypes";
 
 const initialState = {
   reservationData: [],
+  bookReservationData: [],
 };
 
 export default (state = initialState, action) => {
@@ -21,6 +22,24 @@ export default (state = initialState, action) => {
       return {
         ...state,
         reservationData: reservations,
+      };
+    }
+
+    case GET_BOOK_RESERVATIONS_END: {
+      const reservations = action.payload.returnResult.map((reservation) => {
+        return {
+          ...reservation,
+          bookedFrom: reservation.bookedFrom
+            ? reservation.bookedFrom.substring(0, 10)
+            : "",
+          returnDate: reservation.returnDate
+            ? reservation.returnDate.substring(0, 10)
+            : "",
+        };
+      });
+      return {
+        ...state,
+        bookReservationData: reservations,
       };
     }
 
